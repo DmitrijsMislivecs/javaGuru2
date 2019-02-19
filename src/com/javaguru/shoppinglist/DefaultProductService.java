cf2fde442a43306110a5a3626e3aec55e4c24ff7
dms493 <dimis@inbox.lv>
2019-02-19 03:40:50 +0200

    Adding validations

--- src/com/javaguru/shoppinglist/DefaultProductService.java
+++ src/com/javaguru/shoppinglist/DefaultProductService.java
@@ -1,5 +1,6 @@
 package com.javaguru.shoppinglist;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +21,19 @@ public class DefaultProductService implements ProductService {
         if (product == null) {
             throw new IllegalArgumentException("Cannot be null");
         }
+
+        if ((product.getName().length() < 3) || (product.getName().length() > 32)) {
+            throw new IllegalArgumentException("Product name length can not be less 3 symbols or more than 32");
+        }
+
+        if (product.getPrice().compareTo(new BigDecimal(0)) <= 0) {
+            throw new IllegalArgumentException("Product price should be more than 0");
+        }
+
+        if (product.getDiscount().compareTo(new BigDecimal(100)) > 0) {
+            throw new IllegalArgumentException("Discount can not be more than 100%");
+        }
+
         product.setId(PRODUCT_ID_SEQUENCE);
 
         database.put(PRODUCT_ID_SEQUENCE, product);
